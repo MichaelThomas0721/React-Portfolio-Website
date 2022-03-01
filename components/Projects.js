2ad81b2ac69f407887fe82bb1382304da63d5678
Michael <michaelthomas0721@icloud.com>
2022-03-01 11:20:57 -0500

    Bug fixes, updated projects, updated resume

--- components/Projects.js
+++ components/Projects.js
@@ -1,58 +1,136 @@
-import styles from "../styles/projects.module.css"
+import styles from "../styles/projects.module.css";
 import WallPaper from "./../public/Images/WallPaper.gif";
 import StockBot from "./../public/Images/StockBot.gif";
-import AutoProject from "./../public/Images/autoProject.gif";
-import Image from 'next/image';
+import WackyWords from "./../public/Images/WackyWords.gif";
+import Image from "next/image";
 
 export const Projects = () => {
-
-  const myLoader = ({src, quality }) => {
-    return `${src}?q=${quality || 65}`
-  }
+  const myLoader = ({ src, quality }) => {
+    return `${src}?q=${quality || 65}`;
+  };
 
   return (
     <div className={styles.projectContainer}>
       <h1 id={styles.projectsHeader}>Projects</h1>
 
       <div id={styles.firstIContainer} className={styles.imageContainers}>
-        <a href="wall"><Image loader={myLoader} src={WallPaper} alt="Project 1" className={styles.pImages} /></a>
+        <a href="WackyWords">
+          <Image
+            loader={myLoader}
+            src={WackyWords}
+            alt="Project 1"
+            className={styles.pImages}
+          />
+        </a>
       </div>
       <div id={styles.firstTextBox} className={styles.textContainers}>
-        <h3>Stream Deck Wallpaper Generator</h3>
-        <p>Generates 15 cropped gifs using an uploading gif to be used as a wallpaper for the stream deck. The program calculates the middle of the gif
-          and accounts for the spaces inbetween the buttons on the stream deck.
+        <h3>Wacky Words</h3>
+        <p>
+          Wacky Words is a wordle type game where you have a time limit to guess
+          the word, there is a new word every day but you can also choose to
+          generate a new random word. Wacky Words was built using React.js,
+          Next.js, and TailwindCSS
         </p>
         <div>
-           <a href="https://github.com/MichaelThomas0721/WallpaperGeneratorBackend"><svg id={styles.githubIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"/></svg></a>
-        <a href="/wall"><svg id={styles.linkIcon} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"/></svg></a>
+          <a href="https://github.com/MichaelThomas0721/ResumeWebsiteFrontend/blob/main/pages/WackyWords.js">
+            <svg
+              id={styles.githubIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z" />
+            </svg>
+          </a>
+          <a href="/WackyWords">
+            <svg
+              id={styles.linkIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24px"
+              height="24px"
+            >
+              <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z" />
+            </svg>
+          </a>
         </div>
       </div>
 
       <div id={styles.secondIContainer} className={styles.imageContainers}>
-        <a href="https://github.com/MichaelThomas0721/Discord-Stock-Bot"><Image loader={myLoader} src={StockBot} alt="Project 2" className={styles.pImages} /></a>
+        <a href="wall">
+          <Image
+            loader={myLoader}
+            src={WallPaper}
+            alt="Project 2"
+            className={styles.pImages}
+          />
+        </a>
       </div>
       <div id={styles.secondTextBox} className={styles.textContainers}>
-        <h3>Stock Bot</h3>
+        <h3>Stream Deck Wallpaper Generator</h3>
         <p>
-          A stock bot using discord to notify the user that is highly customizable for any product that the user wants to track.
+          Generates 15 cropped gifs using an uploading gif to be used as a
+          wallpaper for the stream deck. The program calculates the middle of
+          the gif and accounts for the spaces inbetween the buttons on the
+          stream deck.
         </p>
         <div>
-        <a href="https://github.com/MichaelThomas0721/Discord-Stock-Bot"><svg id={styles.githubIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"/></svg></a>
+          <a href="https://github.com/MichaelThomas0721/WallpaperGeneratorBackend">
+            <svg
+              id={styles.githubIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z" />
+            </svg>
+          </a>
+          <a href="/wall">
+            <svg
+              id={styles.linkIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24px"
+              height="24px"
+            >
+              <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z" />
+            </svg>
+          </a>
         </div>
       </div>
 
       <div id={styles.thirdIContainer} className={styles.imageContainers}>
-        <a href="https://github.com/MichaelThomas0721/AutoProject"><Image loader={myLoader} src={AutoProject} alt="Project 3" className={styles.pImages}/></a>
+        <a href="https://github.com/MichaelThomas0721/Discord-Stock-Bot">
+          <Image
+            loader={myLoader}
+            src={StockBot}
+            alt="Project 3"
+            className={styles.pImages}
+          />
+        </a>
       </div>
       <div id={styles.thirdTextBox} className={styles.textContainers}>
-        <h3>Auto Project</h3>
-        <p>A python script that automatically creates a new project folder, script, and github repository with just the input of the project name and script name.
-          The script can also be easily integrated into a command prompt command.
+        <h3>Stock Bot</h3>
+        <p>
+          A stock bot using discord to notify the user that is highly
+          customizable for any product that the user wants to track.
         </p>
         <div>
-        <a href="https://github.com/MichaelThomas0721/AutoProject"><svg id={styles.githubIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"/></svg></a>
+          <a href="https://github.com/MichaelThomas0721/Discord-Stock-Bot">
+            <svg
+              id={styles.githubIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z" />
+            </svg>
+          </a>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
