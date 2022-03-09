a197e8c592aae7bc7d6b563652a333a7ad611603
Michael <michaelthomas0721@icloud.com>
2022-03-09 15:24:22 -0500

    Working version

--- pages/RockPaperScissors.js
+++ pages/RockPaperScissors.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function RockPaperScissors() {
   const [aiText, setAiText] = useState("Pick");
-  const [aiMove, setAiMove] = useState();
+  const [userText, setUserText] = useState("");
   const optionsAmount = useRef(3);
   const [options, setOptions] = useState();
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [ties, setTies] = useState(0);
   const emojis = [
     "✊",
     "✋",
     "✌",
-    "🦎",
     "🖖",
+    "🦎",
     "🤟",
     "🖐",
     "👌",
@@ -19,25 +23,30 @@ export default function RockPaperScissors() {
     "👍",
     "👎",
   ];
-  let usedEmojis = [];
+  const usedEmojis = useRef(new Map());
   const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   function changeAiText(input) {
-    let aiInput = Math.floor(Math.random() * optionsAmount.current) + 1;
-    setAiMove(usedEmojis.get(String(aiInput)));
+    let aiInput = Math.floor(Math.random() * optionsAmount.current);
+    let aiMove = (usedEmojis.current.get(String(aiInput)));
+    let aiM = "";
     if (aiInput == input) {
-      setAiText("Tied");
+      aiM = "Tied"
+      setTies(prevTie => prevTie + 1);
     } else if (((aiInput % 2 == 0) == (input % 2 == 0)) == input < aiInput) {
-      setAiText("Won");
-    } else setAiText("Lost");
-    console.log(aiInput);
-    console.log(usedEmojis.get(String(aiInput)));
-    console.log(aiMove);
+      aiM = "Won";
+      setWins(prevWin => prevWin + 1);
+    } else {
+      aiM = "Lost";
+      setLosses(prevLoss => prevLoss + 1);
+    }
+    setAiText("The Ai Picked " + aiMove);
+    setUserText("You picked" + usedEmojis.current.get(String(input)) + " and " + aiM);
   }
 
   function makeOptions() {
     setOptions(
-      Array.from(usedEmojis).map(([key, value]) => {
+      Array.from(usedEmojis.current).map(([key, value]) => {
         return (
           <button
             onClick={() => changeAiText(key)}
@@ -49,28 +58,33 @@ export default function RockPaperScissors() {
         );
       })
     );
-    console.table(options);
   }
 
   function changeOptions(value) {
     if (optionsAmount.current > 3 || value > 0)
       optionsAmount.current = optionsAmount.current + value;
 
-    usedEmojis = new Map();
+    usedEmojis.current = new Map();
     for (let em in emojis.slice(0, optionsAmount.current)){
-      usedEmojis.set(em, emojis[em]);
+      usedEmojis.current.set(em, emojis[em]);
     }
-
-    console.log(usedEmojis);
-    console.log(Object.keys(usedEmojis));
     makeOptions();
   }
 
+  useEffect(() => {
+    changeOptions(0);
+  }, [])
+
   return (
     <div>
-      <div className="left-1/2 top-56 -translate-x-1/2 absolute text-9xl">
-        <h1>The Ai Played {aiMove}</h1>
-        <h1 >You {aiText}</h1>
+      <div className="bg-zinc-400 w-fit text-3xl">
+        <h1>Wins: {wins} </h1><br/>
+        <h1>Losses: {losses}</h1> <br/>
+        <h1>Ties: {ties}</h1>
+      </div>
+      <div className="left-1/2 top-56 -translate-x-1/2 absolute text-7xl">
+        <h1>{aiText}</h1><br/>
+        <h1>{userText}</h1>
       </div>
       <div className="left-1/2 top-optionsTop -translate-x-1/2 absolute">
         {options}
